d6ed688d1da42a13af6837f48ff0d0edca034b0e
Oz Ramos <oz@browsehandsfree.com>
2019-06-13 19:14:34 -0700

    #1 Adds build script, styles, starts facepointer class

--- src/facepointer.js
+++ src/facepointer.js
@@ -1,1 +1,29 @@
+import './styles/main.styl'
+
+// Whether we've loaded dependencies yet
+let loadedDeps = false
+
+/**
+ * 
+ */
+class FacePointer {
+  /**
+   * @param {Object} config The config object (see README)
+   */
+  constructor (config = {}) {
+    this.setup()
+  }
+
+  /**
+   * Sets up variables
+   */
+  setup () {
+    if (!loadedDeps) {
+      loadedDeps = true
+      console.log('loaded')
+    }
+  }
+}
+
+window.FacePointer = FacePointer
 console.log('(∩｀-´)⊃━☆ﾟ.*・｡ﾟ https://github.com/browsehandsfree/facepointer')
