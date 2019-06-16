75334e5ff68a5a4cc0439c12cade3d12bfe75342
Oz Ramos <oz@browsehandsfree.com>
2019-06-16 08:52:59 -0700

    #6 minor code reorganization

--- integrations/wordpress/facepointer-wp.php
+++ integrations/wordpress/facepointer-wp.php
@@ -6,15 +6,12 @@
  * Author: Oz Ramos
  * Author URI: https://browsehandsfree.com
  */
-if (!defined('WPINC')) {
-	die;
-}
 
 /**
  * Include scripts
  */
-add_action('wp_enqueue_scripts', 'facepointer_scripts');
 function facepointer_scripts() {
-  wp_enqueue_script('facepointer', 'https://unpkg.com/facepointer@0.0.4/dist/facepointer.js', array(), null, true);
+  wp_enqueue_script('facepointer', 'https://unpkg.com/facepointer@0.0.4/dist/facepointer.js', [], false, true);
   wp_enqueue_style('facepointer', 'https://unpkg.com/facepointer@0.0.4/dist/facepointer.css');
 }
+add_action('wp_enqueue_scripts', 'facepointer_scripts');
